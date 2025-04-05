91b79b3aa92cec7d98f63b1a48b6b0c6b0325316
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-04-06 01:44:57 +0200

    feat(bibitem): implement bibkey formatting function and add corresponding tests

---
+++ philoch_bib_sdk/converters/plaintext/bibitem/bibkey_formatter.py
@@ -0,0 +1,18 @@
+from philoch_bib_sdk.logic.models import BibKeyAttr
+
+
+def format_bibkey(bibkey: BibKeyAttr) -> str:
+
+    if bibkey.other_authors:
+        authors_l = [bibkey.first_author, bibkey.other_authors]
+    else:
+        authors_l = [bibkey.first_author]
+
+    authors = "-".join(authors_l)
+
+    if isinstance(bibkey.date, int):
+        year = f"{bibkey.date}{bibkey.date_suffix}"
+    else:
+        year = f"{bibkey.date}-{bibkey.date_suffix}" if bibkey.date_suffix else bibkey.date
+
+    return f"{authors}:{year}"
